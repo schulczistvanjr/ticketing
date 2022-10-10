e2f452e6360e0ed790177e923aed45d39404c2ed
Istvan Schulcz <schulczistvanjr@gmail.com>
2022-10-10 11:53:02 +0200

    order service routes and tests

---
+++ orders/src/routes/__test__/show.test.ts
@@ -0,0 +1,49 @@
+import request from "supertest";
+import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
+
+it("fetches the order", async () => {
+  const ticket = Ticket.build({
+    title: "Ticket title",
+    price: 20,
+  });
+  await ticket.save();
+
+  const user = signin();
+
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  const { body: fetchedOrder } = await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set("Cookie", user)
+    .send()
+    .expect(200);
+  expect(fetchedOrder.id).toEqual(order.id);
+});
+
+it("returns an error is one user tries to fetch other users order", async () => {
+  const ticket = Ticket.build({
+    title: "Ticket title",
+    price: 20,
+  });
+  await ticket.save();
+
+  const user = signin();
+
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  const { body: fetchedOrder } = await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set("Cookie", signin())
+    .send()
+    .expect(401);
+  expect(fetchedOrder.errors[0].message).toEqual("Not authorized");
+});
