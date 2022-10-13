f8e14b374203c061ee1d488dc0644930369ab987
Steve <schulczistvanjr@gmail.com>
2022-10-13 11:19:17 +0200

    removed console log (#2)

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -28,7 +28,6 @@ const start = async () => {
   }
 
   try {
-    console.log("Starting up...");
     await natsWrapper.connect(
       process.env.NATS_CLUSTER_ID,
       process.env.NATS_CLIENT_ID,
